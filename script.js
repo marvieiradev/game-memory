03dc7e61256cc5cfb7ad728a581e500e733b0d2b
Marcos Vieira <marcosvieira482@gmail.com>
2023-12-12 18:22:22 -0300

    feat: game time adjustment

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const cards = document.querySelectorAll(".card"),
     flipsTag = document.querySelector(".flips b"),
     refreshBtn = document.querySelector(".details button");
 
-let maxTime = 20;
+let maxTime = 30;
 let timeLeft = maxTime;
 let flips = 0;
 let matchedCards = 0;
